88f8a229b09355fa1a87f816462ebb77c2fc9867
ericnoharaleclair <eric.noharaleclair@gmail.com>
2023-12-05 13:41:55 -0500

    fixed movement

--- turkey-evade.py
+++ turkey-evade.py
@@ -3,52 +3,11 @@ import pygame as pg
 import random
 
 pg.init()
-####################################################################################################################
-# CLASSES
-####################################################################################################################
-
-class Player(object):
-    def __init__(self):
-        self.image = pg.image.load("assets/turkey.gif")
-        self.rect = self.image.get_rect(center=(WIDTH/2, HEIGHT/2))
-
-    def draw(self):
-        screen.fill(black)
-        screen.blit(self.image, self.rect)
-
-    def update(self, direction):
-        self.rect.x += direction[0] * 300 * DT
-        self.rect.y += direction[1] * 300 * DT
-        self.draw()
-    
-    def handle_keys(self):
-        for event in pg.event.get():
-            keys = pg.key.get_pressed()
-            if event.type == pg.QUIT:
-                pg.quit()
-                sys.exit()
-            elif event.type == pg.KEYDOWN:
-                if keys[pg.K_UP] or keys[pg.K_w]:
-                    self.update(UP)
-                elif keys[pg.K_DOWN] or keys[pg.K_s]:
-                    self.update(DOWN)
-                elif keys[pg.K_LEFT] or keys[pg.K_a]:
-                    self.update(LEFT)
-                elif keys[pg.K_RIGHT] or keys[pg.K_d]:
-                    self.update(RIGHT)
-                elif keys[pg.K_ESCAPE]:
-                    pg.quit()
-                    sys.exit()
 
 ####################################################################################################################
 # VARIABLES
 ####################################################################################################################
 SIZE = WIDTH, HEIGHT = 1280, 720
-UP = (0,-1)
-DOWN = (0,1)
-LEFT = (-1,0)
-RIGHT = (1,0)
-DT = 0.05
 
 screen = pg.display.set_mode(SIZE)      # set the screen
 font = pg.font.Font('freesansbold.ttf', 30)
@@ -61,25 +20,55 @@ red = (255,0,0)
 white = (255,255,255)
 
 ####################################################################################################################
+# CLASSES
+####################################################################################################################
+
+class Player(object):
+    def __init__(self):
+        self.image = pg.image.load("assets/turkey.gif")
+        self.rect = self.image.get_rect(center=(WIDTH/2, HEIGHT/2))
+
+    def draw(self):
+        screen.fill(black)
+        screen.blit(self.image, self.rect)
+   
+    def handle_keys(self):
+        keys = pg.key.get_pressed()
+        move = 10 if keys[pg.K_LSHIFT] else 5
+        if keys[pg.K_UP] or keys[pg.K_w]:
+            self.rect.y -= move
+        if keys[pg.K_DOWN] or keys[pg.K_s]:
+            self.rect.y += move
+        if keys[pg.K_LEFT] or keys[pg.K_a]:
+            self.rect.x -= move
+        if keys[pg.K_RIGHT] or keys[pg.K_d]:
+            self.rect.x += move
+
+        
+
+####################################################################################################################
 # GAME LOOP
 ####################################################################################################################
 
 def play():
     pg.display.set_caption("Play")
-    # surface = pg.Surface(screen.get_size()).convert()     #create surface to draw on
     screen.fill(black)
 
     player = Player()
     # bullet = Bullet()
 
-    screen.blit(player.image, player.rect)
+    player.draw()
 
     # Gameplay loop
     while True:
-        clock.tick(60)      #60 FPS game
-        # player.draw()
-        player.handle_keys()
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
 
-        pg.display.update()
+        clock.tick(60)      #60 FPS game
+        player.handle_keys()
+        player.draw()
+        pg.display.flip()
 
 play()
